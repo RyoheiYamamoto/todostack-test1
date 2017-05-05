590ed6e6ae75641352eb653807cef695dd523ac8
Ryo-chin <h031203yama@gmail.com>
2017-05-05 12:01:59 +0900

    add Routing and each component

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { TaskComponent } from './task/task.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { WorkComponent } from './work/work.component';
+import { SettingComponent } from './setting/setting.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/tasks', pathMatch: 'full' },
+  { path: 'tasks', component: TaskComponent },
+  { path: 'works', component: WorkComponent },
+  { path: 'settings', component: SettingComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,28 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { TaskComponent } from './task/task.component';
 import { TaskService } from './task.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { WorkComponent } from './work/work.component';
+import { SettingComponent } from './setting/setting.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskComponent
+    TaskComponent,
+    PageNotFoundComponent,
+    WorkComponent,
+    SettingComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [TaskService],
   bootstrap: [AppComponent]
